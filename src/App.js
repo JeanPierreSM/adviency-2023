cbb61cf8de28b874e6a734935777f4c8372ddbf8
jeanpierresm52 <jeanpierre.saintmartin@overproofdev.com>
2023-12-08 16:29:41 -0300

    Chakra UI / Prettier installed. Initial project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import MainContainer from "./day1/components/MainContainer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Regalos:</h1>
+      <MainContainer />
+    </div>
+  );
+}
